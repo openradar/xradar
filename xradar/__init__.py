62020cec866904d9c7368aa57c6fa843ca6226b1
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2022-09-01 11:27:23 +0200

    ADD: versioning

--- xradar/__init__.py
+++ xradar/__init__.py
@@ -2,4 +2,12 @@
 
 __author__ = """Maxwell Grover"""
 __email__ = 'mgroverwx@gmail.com'
-__version__ = '0.1.0'
+
+# versioning
+try:
+    from .version import version as __version__
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
+
