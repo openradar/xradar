3fce1581803a484e5c445309ddb995902bdc95df
Max Grover <maxi2312@gmail.com>
2022-09-01 11:53:57 +0200

    update setup file to read markdown (#6)

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='xradar',
     name='xradar',
