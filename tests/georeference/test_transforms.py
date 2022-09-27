8d686716901ed5ea42853e0dea023de517b69ec8
mgrover1 <mgroverwx@gmail.com>
2022-09-27 15:22:08 -0500

    update georeference module and add precommit books

--- tests/georeference/test_transforms.py
+++ tests/georeference/test_transforms.py
@@ -1,9 +1,9 @@
-import warnings
-
 import numpy as np
+import pandas as pd
 from numpy.testing import assert_almost_equal
 
-from xradar.georeference import antenna_to_cartesian, geographic_to_cartesian_aeqd
+import xradar
+from xradar.georeference import antenna_to_cartesian, get_x_y_z
 
 
 def test_antenna_to_cartesian():
@@ -20,46 +20,37 @@ def test_antenna_to_cartesian():
     assert_almost_equal(z[0], 0)
 
     # Make sure that at 180 degrees, x is (close to) 0
-    assert_almost_equal(x[np.where(azimuths == 180.)], 0)
+    assert_almost_equal(x[np.where(azimuths == 180.0)], 0)
 
     # Make sure that at 270 degrees, y is (close to) 0
-    assert_almost_equal(y[np.where(azimuths == 270.)], 0)
+    assert_almost_equal(y[np.where(azimuths == 270.0)], 0)
 
 
-def test_geographic_to_cartesian_aeqd():
-    # Example taken from:
-    # Snyder, J.P. Map Projections A Working Manual, 1987, page 338.
-    R = 3.0
-    lat_0 = 40.0        # 40 degrees North latitude
-    lon_0 = -100.       # 100 degrees West longitude
-    lat = -20.0         # 20 degrees S latitude
-    lon = 100.0         # 100.0 E longitude
-    x = -5.8311398
-    y = 5.5444634
+def test_get_x_y_z():
 
-    with warnings.catch_warnings():  # invalid divide is handled by code
-        warnings.simplefilter('ignore', category=RuntimeWarning)
-        x, y = geographic_to_cartesian_aeqd(
-            lon, lat, lon_0, lat_0, R)
-    assert_almost_equal(x, -5.8311398, 7)
-    assert_almost_equal(y, 5.5444634, 7)
+    # Create an empty xradar dataset
+    ds = xradar.model.Dataset()
 
-    # edge case, distance from projection center is zero
-    lat = 40.0
-    lon = -100.
-    with warnings.catch_warnings():  # invalid divide is handled by code
-        # ignore division runtime warning
-        warnings.simplefilter('ignore', category=RuntimeWarning)
-        x, y = geographic_to_cartesian_aeqd(
-            lon, lat, lon_0, lat_0, R)
-    assert_almost_equal(x, 0.0, 5)
-    assert_almost_equal(y, 0.0, 5)
+    # Add similar data to before
+    ds['time'] = pd.date_range(start='2018-11-10', end='2018-11-10T00:09:00', freq='T')
+    ds['range'] = np.arange(0, 1000, 100)
+    ds['azimuth'] = ('time', np.arange(0, 300, 30))
+    ds['elevation'] = ('time', np.arange(0, 50, 5))
 
-    # edge case, sin(c) is zero
-    with warnings.catch_warnings():  # invalid divide is handled by code
-        warnings.simplefilter('ignore', category=RuntimeWarning)
-        x, y = geographic_to_cartesian_aeqd(
-            10.0, 90.0, 20.0, 90.0, 3.0)
+    ds = get_x_y_z(ds)
 
-    assert_almost_equal(x, 0.0, 5)
-    assert_almost_equal(y, 0.0, 5)
+    # Check to see if the origin contains all 0s
+    origin = ds.isel(range=0, time=0)
+    assert_almost_equal(origin.x, 0)
+    assert_almost_equal(origin.y, 0)
+    assert_almost_equal(origin.z, 0)
+
+    # Make sure that at 180 degrees, x is (close to) 0
+    np.testing.assert_almost_equal(
+        ds.isel(range=0, time=np.where(ds.azimuth == 180)[0]).x, 0
+    )
+
+    # Make sure that at 270 degrees, y is (close to) 0
+    np.testing.assert_almost_equal(
+        ds.isel(range=0, time=np.where(ds.azimuth == 270)[0]).y, 0
+    )
--- xradar/georeference/transforms.py
+++ xradar/georeference/transforms.py
@@ -2,12 +2,12 @@
 # Copyright (c) 2022, openradar developers.
 # Distributed under the MIT License. See LICENSE for more info.
 
-import warnings
-
 import numpy as np
 
 
-def antenna_to_cartesian(ranges, azimuths, elevations):
+def antenna_to_cartesian(
+    ranges, azimuths, elevations, earth_radius=6371000, effective_radius_fraction=None
+):
     """
     Return Cartesian coordinates from antenna coordinates.
     Parameters
@@ -18,6 +18,10 @@ def antenna_to_cartesian(ranges, azimuths, elevations):
         Azimuth angle of the radar in degrees.
     elevations : array
         Elevation angle of the radar in degrees.
+    earth_radius: float
+        Radius of the earth (default is 6371000 m).
+    effective_radius_fraction: float
+        Fraction of earth to use for the effective radius (default is 4/3).
     Returns
     -------
     x, y, z : array
@@ -41,79 +45,72 @@ def antenna_to_cartesian(ranges, azimuths, elevations):
     .. [1] Doviak and Zrnic, Doppler Radar and Weather Observations, Second
         Edition, 1993, p. 21.
     """
+    if effective_radius_fraction is None:
+        effective_radius_fraction = 4.0 / 3.0
     theta_e = np.deg2rad(elevations)  # elevation angle in radians.
     theta_a = np.deg2rad(azimuths)  # azimuth angle in radians.
-    R = 6371.0 * 1000.0 * 4.0 / 3.0     # effective radius of earth in meters.
-    r = ranges * 1000.0                 # distances to gates in meters.
+    R = earth_radius * effective_radius_fraction  # effective radius of earth in meters.
+    r = ranges  # distances to gates in meters.
 
-    z = (r ** 2 + R ** 2 + 2.0 * r * R * np.sin(theta_e)) ** 0.5 - R
+    z = (r**2 + R**2 + 2.0 * r * R * np.sin(theta_e)) ** 0.5 - R
     s = R * np.arcsin(r * np.cos(theta_e) / (R + z))  # arc length in m.
     x = s * np.sin(theta_a)
     y = s * np.cos(theta_a)
     return x, y, z
 
 
-def geographic_to_cartesian_aeqd(lon, lat, lon_0, lat_0, R=6370997.):
+def get_x_y_z(ds, earth_radius=6371000, effective_radius_fraction=None):
     """
-    Azimuthal equidistant geographic to Cartesian coordinate transform.
-    Transform a set of geographic coordinates (lat, lon) to
-    Cartesian/Cartographic coordinates (x, y) using a azimuthal equidistant
-    map projection [1]_.
-    .. math::
-        x = R * k * \\cos(lat) * \\sin(lon - lon_0)
-        y = R * k * [\\cos(lat_0) * \\sin(lat) -
-                     \\sin(lat_0) * \\cos(lat) * \\cos(lon - lon_0)]
-        k = c / \\sin(c)
-        c = \\arccos(\\sin(lat_0) * \\sin(lat) +
-                     \\cos(lat_0) * \\cos(lat) * \\cos(lon - lon_0))
-    Where x, y are the Cartesian position from the center of projection;
-    lat, lon the corresponding latitude and longitude; lat_0, lon_0 are the
-    latitude and longitude of the center of the projection; R is the radius of
-    the earth (defaults to ~6371 km).
+    Return Cartesian coordinates from antenna coordinates.
     Parameters
     ----------
-    lon, lat : array-like
-        Longitude and latitude coordinates in degrees.
-    lon_0, lat_0 : float
-        Longitude and latitude, in degrees, of the center of the projection.
-    R : float, optional
-        Earth radius in the same units as x and y. The default value is in
-        units of meters.
+    ds: xarray.Dataset
+        Xarray dataset containing range, azimuth, and elevation
+    earth_radius: float
+        Radius of the earth (default is 6371000 m).
+    effective_radius_fraction: float
+        Fraction of earth to use for the effective radius (default is 4/3).
     Returns
     -------
-    x, y : array
-        Cartesian coordinates in the same units as R, typically meters.
+    ds : xarray.Dataset
+        Dataset including x, y, and z as coordinates.
+    Notes
+    -----
+    The calculation for Cartesian coordinate is adapted from equations
+    2.28(b) and 2.28(c) of Doviak and Zrnic [1]_ assuming a
+    standard atmosphere (4/3 Earth's radius model).
+    .. math::
+        z = \\sqrt{r^2+R^2+2*r*R*sin(\\theta_e)} - R
+        s = R * arcsin(\\frac{r*cos(\\theta_e)}{R+z})
+        x = s * sin(\\theta_a)
+        y = s * cos(\\theta_a)
+    Where r is the distance from the radar to the center of the gate,
+    :math:`\\theta_a` is the azimuth angle, :math:`\\theta_e` is the
+    elevation angle, s is the arc length, and R is the effective radius
+    of the earth, taken to be 4/3 the mean radius of earth (6371 km).
     References
     ----------
-    .. [1] Snyder, J. P. Map Projections--A Working Manual. U. S. Geological
-        Survey Professional Paper 1395, 1987, pp. 191-202.
+    .. [1] Doviak and Zrnic, Doppler Radar and Weather Observations, Second
+        Edition, 1993, p. 21.
     """
-    lon = np.atleast_1d(np.asarray(lon))
-    lat = np.atleast_1d(np.asarray(lat))
 
-    lon_rad = np.deg2rad(lon)
-    lat_rad = np.deg2rad(lat)
+    # Calculate x, y, and z from the dataset
+    ds['x'], ds['y'], ds['z'] = antenna_to_cartesian(
+        ds.range,
+        ds.azimuth,
+        ds.elevation,
+        earth_radius=earth_radius,
+        effective_radius_fraction=None,
+    )
 
-    lat_0_rad = np.deg2rad(lat_0)
-    lon_0_rad = np.deg2rad(lon_0)
+    # Set the attributes for the dataset
+    ds.x.attrs = {'standard_name': 'east_west_distance_from_radar', 'units': 'meters'}
 
-    lon_diff_rad = lon_rad - lon_0_rad
+    ds.y.attrs = {'standard_name': 'north_south_distance_from_radar', 'units': 'meters'}
 
-    # calculate the arccos after ensuring all values in valid domain, [-1, 1]
-    arg_arccos = (np.sin(lat_0_rad) * np.sin(lat_rad) +
-                  np.cos(lat_0_rad) * np.cos(lat_rad) * np.cos(lon_diff_rad))
-    arg_arccos[arg_arccos > 1] = 1
-    arg_arccos[arg_arccos < -1] = -1
-    c = np.arccos(arg_arccos)
-    with warnings.catch_warnings():
-        # division by zero may occur here but is properly addressed below so
-        # the warnings can be ignored
-        warnings.simplefilter('ignore', RuntimeWarning)
-        k = c / np.sin(c)
-    # fix cases where k is undefined (c is zero), k should be 1
-    k[c == 0] = 1
+    ds.z.attrs = {'standard_name': 'height_above_ground', 'units': 'meters'}
 
-    x = R * k * np.cos(lat_rad) * np.sin(lon_diff_rad)
-    y = R * k * (np.cos(lat_0_rad) * np.sin(lat_rad) -
-                 np.sin(lat_0_rad) * np.cos(lat_rad) * np.cos(lon_diff_rad))
-    return x, y
+    # Make sure these
+    ds.set_coords(['x', 'y', 'z'])
+
+    return ds
