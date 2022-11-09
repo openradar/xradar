507f112248831a6856df2055023e6b2fbdbeb51e
mgrover1 <mgroverwx@gmail.com>
2022-11-09 13:12:24 -0600

    fix failing tests and formatting

--- xradar/io/backends/cfradial1.py
+++ xradar/io/backends/cfradial1.py
@@ -26,11 +26,11 @@ Example::
 """
 
 __all__ = [
-    'CfRadial1BackendEntrypoint',
-    'open_cfradial1_datatree',
+    "CfRadial1BackendEntrypoint",
+    "open_cfradial1_datatree",
 ]
 
-__doc__ = __doc__.format('\n   '.join(__all__))
+__doc__ = __doc__.format("\n   ".join(__all__))
 
 from datatree import DataTree
 from xarray import open_dataset
@@ -58,7 +58,7 @@ def _get_required_root_dataset(ds):
     return root
 
 
-def _get_sweep_groups(root, sweep=None, first_dim='time'):
+def _get_sweep_groups(root, sweep=None, first_dim="time"):
     """Extract Sweep Groups.
 
     Ported from wradlib.
@@ -66,8 +66,8 @@ def _get_sweep_groups(root, sweep=None, first_dim='time'):
     # get hold of sweep start/stop indices
     start_idx = root.sweep_start_ray_index.values.astype(int)
     end_idx = root.sweep_end_ray_index.values.astype(int)
-    ray_n_gates = root.get('ray_n_gates', False)
-    ray_start_index = root.get('ray_start_index', False)
+    ray_n_gates = root.get("ray_n_gates", False)
+    ray_start_index = root.get("ray_start_index", False)
 
     # strip attributes
     data = root
@@ -76,26 +76,26 @@ def _get_sweep_groups(root, sweep=None, first_dim='time'):
     if isinstance(sweep, str):
         sweep = [sweep]
     elif isinstance(sweep, int):
-        sweep = [f'sweep_{sweep}']
+        sweep = [f"sweep_{sweep}"]
 
     # iterate over sweeps
-    for i in range(root.dims['sweep']):
-        sw = f'sweep_{i}'
+    for i in range(root.dims["sweep"]):
+        sw = f"sweep_{i}"
         if sweep is not None and not (sw in sweep or i in sweep):
             continue
 
         # slice time and sweep dimension
         tslice = slice(start_idx[i], end_idx[i] + 1)
         swslice = slice(i, i + 1)
-        ds = data.isel(time=tslice, sweep=swslice).squeeze('sweep')
+        ds = data.isel(time=tslice, sweep=swslice).squeeze("sweep")
 
         sweep_mode = _maybe_decode(ds.sweep_mode).compute()
-        dim0 = 'elevation' if sweep_mode == 'rhi' else 'azimuth'
+        dim0 = "elevation" if sweep_mode == "rhi" else "azimuth"
 
         # check and extract for variable number of gates
         if ray_n_gates is not False:
             # swap dimensions to correctly stack/unstack n_points = ["time", "range"]
-            ds = ds.swap_dims({'time': dim0})
+            ds = ds.swap_dims({"time": dim0})
 
             current_ray_n_gates = ray_n_gates.isel(time=tslice)
             current_rays_sum = current_ray_n_gates.sum().values.astype(int)
@@ -106,31 +106,31 @@ def _get_sweep_groups(root, sweep=None, first_dim='time'):
             rslice = slice(0, current_ray_n_gates[0].values.astype(int))
             ds = ds.isel(range=rslice)
             ds = ds.isel(n_points=nslice)
-            ds = ds.stack(n_points=[dim0, 'range'])
-            ds = ds.unstack('n_points')
+            ds = ds.stack(n_points=[dim0, "range"])
+            ds = ds.unstack("n_points")
             # fix elevation/time additional range dimension in coordinate
-            ds = ds.assign_coords({'elevation': ds.elevation.isel(range=0, drop=True)})
+            ds = ds.assign_coords({"elevation": ds.elevation.isel(range=0, drop=True)})
 
         # handling first dimension
-        if first_dim == 'auto':
-            if 'time' in ds.dims:
-                ds = ds.swap_dims({'time': dim0})
+        if first_dim == "auto":
+            if "time" in ds.dims:
+                ds = ds.swap_dims({"time": dim0})
             ds = ds.sortby(dim0)
         else:
-            if 'time' not in ds.dims:
-                ds = ds.swap_dims({dim0: 'time'})
-            ds = ds.sortby('time')
+            if "time" not in ds.dims:
+                ds = ds.swap_dims({dim0: "time"})
+            ds = ds.sortby("time")
 
         # reassign azimuth/elevation coordinates
-        ds = ds.assign_coords({'azimuth': ds.azimuth})
-        ds = ds.assign_coords({'elevation': ds.elevation})
+        ds = ds.assign_coords({"azimuth": ds.azimuth})
+        ds = ds.assign_coords({"elevation": ds.elevation})
 
         # assign geo-coords
         ds = ds.assign_coords(
             {
-                'latitude': root.latitude,
-                'longitude': root.longitude,
-                'altitude': root.altitude,
+                "latitude": root.latitude,
+                "longitude": root.longitude,
+                "altitude": root.altitude,
             }
         )
 
@@ -139,7 +139,7 @@ def _get_sweep_groups(root, sweep=None, first_dim='time'):
     return sweep_groups
 
 
-def _assign_data_radial(root, sweep='sweep_0', first_dim='time'):
+def _assign_data_radial(root, sweep="sweep_0", first_dim="time"):
     """Assign from CfRadial1 data structure.
 
     Parameters
@@ -185,13 +185,13 @@ def open_cfradial1_datatree(filename_or_obj, **kwargs):
         DataTree
     """
     # handle kwargs, extract first_dim
-    first_dim = kwargs.get('first_dim', None)
-    sweep = kwargs.pop('sweep', None)
+    first_dim = kwargs.get("first_dim", None)
+    sweep = kwargs.pop("sweep", None)
 
     # open root group, cfradial1 only has one group
-    ds = open_dataset(filename_or_obj, engine='cfradial1', **kwargs)
+    ds = open_dataset(filename_or_obj, engine="cfradial1", **kwargs)
     # create datatree root node with required data
-    dtree = DataTree(data=_get_required_root_dataset(ds), name='root')
+    dtree = DataTree(data=_get_required_root_dataset(ds), name="root")
     # return datatree with attached sweep child nodes
     return _attach_sweep_groups(
         dtree, _get_sweep_groups(ds, sweep=sweep, first_dim=first_dim)
@@ -222,8 +222,8 @@ class CfRadial1BackendEntrypoint(BackendEntrypoint):
         use_cftime=None,
         decode_timedelta=None,
         format=None,
-        group='/',
-        first_dim='time',
+        group="/",
+        first_dim="time",
     ):
 
         store = NetCDF4DataStore.open(
@@ -245,11 +245,11 @@ class CfRadial1BackendEntrypoint(BackendEntrypoint):
             decode_timedelta=decode_timedelta,
         )
 
-        if group != '/':
+        if group != "/":
             ds = _assign_data_radial(ds, sweep=group, first_dim=first_dim)
             if not ds:
                 raise ValueError(
-                    f'Group `{group}` missing from file `{filename_or_obj}`.'
+                    f"Group `{group}` missing from file `{filename_or_obj}`."
                 )
             ds = ds[0]
         return ds
