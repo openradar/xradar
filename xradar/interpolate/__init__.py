045ba9962cd8e20a403d43ea99349a72128273a2
mgrover1 <mgroverwx@gmail.com>
2024-01-25 16:28:03 -0600

    make sure submodule is blank

--- xradar/interpolate/__init__.py
+++ xradar/interpolate/__init__.py
@@ -6,10 +6,3 @@
 XRadar Interpolation
 ====================
 """
-
-from ._nexrad_interpolate import (
-    _fast_interpolate_scan_2,  # noqa
-    _fast_interpolate_scan_4,  # noqa
-)
-
-__all__ = [s for s in dir() if not s.startswith("_")]
