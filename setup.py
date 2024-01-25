2f265658354be95be9d1e5534ab07a85a13efe33
mgrover1 <mgroverwx@gmail.com>
2024-01-25 16:29:09 -0600

    ADD: Add extra line

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import numpy
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 from setuptools import Extension, setup
@@ -12,13 +11,13 @@ extensions = [
     Extension(
         "xradar.interpolate._nexrad_interpolate",
         sources=["xradar/interpolate/_nexrad_interpolate.pyx"],
-        include_dirs=[numpy.get_include()],
     ),
 ]
 
 
 # This is the function that is executed
 setup(
+    install_requires=[""],
     # external to be compiled
     ext_modules=cythonize(
         extensions, compiler_directives={"language_level": "3", "cpow": True}
