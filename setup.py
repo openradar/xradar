a904c0312b32152e94db71b9aa04bca656c77a65
mgrover1 <mgroverwx@gmail.com>
2024-01-25 14:28:48 -0600

    FIX: Fix build of cython extensions

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ extensions = [
 
 # This is the function that is executed
 setup(
-    name="xradar",  # Required
     # external to be compiled
     ext_modules=cythonize(
         extensions, compiler_directives={"language_level": "3", "cpow": True}
