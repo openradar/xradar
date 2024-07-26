a94926da4c5452cbfebc791bd41031557deea2b9
Bobby Jackson <rjackson@anl.gov>
2024-07-25 19:29:10 -0500

    FIX: Style issues

--- tests/io/test_hpl.py
+++ tests/io/test_hpl.py
@@ -1,14 +1,13 @@
-import numpy as np
-import pytest
 
 import xarray as xr
+from open_radar_data import DATASETS
+
 import xradar as xd
 
-from open_radar_data import DATASETS
 
 def test_open_datatree_hpl():
     ds = xd.io.open_hpl_datatree(DATASETS.fetch('User1_184_20240601_013257.hpl'),
-                             sweep=[0,1,2,3,4,5,6,7,8], 
+                             sweep=[0,1,2,3,4,5,6,7,8],
                              backend_kwargs=dict(
                                 latitude=41.24276244459537, longitude=-70.1070364814594))
     assert '/sweep_0' in list(ds.groups)
@@ -20,6 +19,6 @@ def test_open_dataset_hpl():
                              engine='hpl',
                              backend_kwargs=dict(
                                 latitude=41.24276244459537, longitude=-70.1070364814594))
-    
+
     assert ds['mean_doppler_velocity'].dims == ('time', 'range')
     assert ds['mean_doppler_velocity'].max() == 19.5306
