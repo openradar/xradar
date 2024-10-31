f731eb8ef33863d8bae461205fa6d7c56782cd95
aladinor <aladinor@unal.edu.co>
2024-10-31 10:48:08 -0500

    using radar calibration subgrup fucntion from common.py in iris backend

--- xradar/io/backends/iris.py
+++ xradar/io/backends/iris.py
@@ -43,7 +43,7 @@ from collections import OrderedDict
 
 import numpy as np
 import xarray as xr
-from xarray import Dataset, DataTree
+from xarray import DataTree
 from xarray.backends.common import AbstractDataStore, BackendArray, BackendEntrypoint
 from xarray.backends.file_manager import CachingFileManager
 from xarray.backends.store import StoreBackendEntrypoint
@@ -61,10 +61,16 @@ from ...model import (
     get_longitude_attrs,
     get_range_attrs,
     moment_attrs,
+    radar_calibration_subgroup,
     radar_parameters_subgroup,
     sweep_vars_mapping,
 )
-from .common import _attach_sweep_groups, _get_required_root_dataset, _get_subgroup
+from .common import (
+    _attach_sweep_groups,
+    _get_radar_calibration,
+    _get_required_root_dataset,
+    _get_subgroup,
+)
 
 #: mapping from IRIS names to CfRadial2/ODIM
 iris_mapping = {
@@ -3967,12 +3973,6 @@ def _get_iris_group_names(filename):
     return keys
 
 
-def _get_radar_calibration(ls_ds: list[xr.Dataset]) -> xr.Dataset:
-    """Get radar calibration root metadata group."""
-    # radar_calibration is connected with calib-dimension
-    return Dataset()
-
-
 class IrisBackendEntrypoint(BackendEntrypoint):
     """Xarray BackendEntrypoint for IRIS/Sigmet data."""
 
@@ -4120,7 +4120,7 @@ def open_iris_datatree(filename_or_obj, **kwargs):
         "/georeferencing_correction": _get_subgroup(
             ls_ds, georeferencing_correction_subgroup
         ),
-        "/radar_calibration": _get_radar_calibration(ls_ds),
+        "/radar_calibration": _get_radar_calibration(ls_ds, radar_calibration_subgroup),
     }
     dtree = _attach_sweep_groups(dtree, ls_ds)
     return DataTree.from_dict(dtree)
