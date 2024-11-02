2624bfe66eaa24e06e2f3d44f86ccad4006a115d
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2024-11-02 18:34:19 +0100

    FIX: make objects mergable, don't raise when attempting to drop missing variables

--- xradar/io/export/cfradial1.py
+++ xradar/io/export/cfradial1.py
@@ -57,9 +57,8 @@ def _calib_mapper(calib_params):
             attrs=data_array.attrs,
         )
     radar_calib_renamed = xr.Dataset(new_data_vars)
-    dummy_ds = radar_calib_renamed.rename_vars({"r_calib": "fake_coord"})
-    del dummy_ds["fake_coord"]
-    return dummy_ds
+    radar_calib_renamed = radar_calib_renamed.drop_vars("r_calib", errors="ignore")
+    return radar_calib_renamed
 
 
 def _main_info_mapper(dtree):
@@ -140,7 +139,7 @@ def _variable_mapper(dtree, dim0=None):
         dim="time",
         compat="no_conflicts",
         join="outer",
-        combine_attrs="drop_conflicts",
+        combine_attrs="no_conflicts",
     )
 
     drop_variables = [
@@ -304,11 +303,11 @@ def to_cfradial1(dtree=None, filename=None, calibs=True):
 
     # Add additional parameters if they exist in dtree
     if "radar_parameters" in dtree:
-        radar_params = dtree["radar_parameters"].to_dataset()
+        radar_params = dtree["radar_parameters"].to_dataset().reset_coords()
         dataset.update(radar_params)
 
     if "georeferencing_correction" in dtree:
-        radar_georef = dtree["georeferencing_correction"].to_dataset()
+        radar_georef = dtree["georeferencing_correction"].to_dataset().reset_coords()
         dataset.update(radar_georef)
 
     # Ensure that the data type of sweep_mode and similar variables matches
