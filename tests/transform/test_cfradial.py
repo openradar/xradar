253c5f0dc3defddce16d845d3e9a9dea64e419c2
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2024-11-03 15:26:32 +0100

    FIX: lint

--- tests/transform/test_cfradial.py
+++ tests/transform/test_cfradial.py
@@ -18,8 +18,12 @@ def test_to_cfradial1(cfradial1_file):
 
         # Verify key attributes and data structures in the resulting dataset
         assert isinstance(ds_cf1, xr.Dataset), "Output is not a valid xarray Dataset"
-        assert "Conventions" in ds_cf1.attrs and ds_cf1.attrs["Conventions"] == "Cf/Radial"
-        assert "sweep_mode" in ds_cf1.variables, "Missing sweep_mode in converted dataset"
+        assert (
+            "Conventions" in ds_cf1.attrs and ds_cf1.attrs["Conventions"] == "Cf/Radial"
+        )
+        assert (
+            "sweep_mode" in ds_cf1.variables
+        ), "Missing sweep_mode in converted dataset"
         assert ds_cf1.attrs["version"] == "1.2", "Incorrect CfRadial version"
 
 
@@ -44,10 +48,14 @@ def test_to_cfradial1_with_different_range_shapes(nexradlevel2_bzfile):
         ds_cf1 = xd.transform.to_cfradial1(dtree)
         # Verify key attributes and data structures in the resulting dataset
         assert isinstance(ds_cf1, xr.Dataset), "Output is not a valid xarray Dataset"
-        assert "Conventions" in ds_cf1.attrs and ds_cf1.attrs["Conventions"] == "Cf/Radial"
-        assert "sweep_mode" in ds_cf1.variables, "Missing sweep_mode in converted dataset"
+        assert (
+            "Conventions" in ds_cf1.attrs and ds_cf1.attrs["Conventions"] == "Cf/Radial"
+        )
+        assert (
+            "sweep_mode" in ds_cf1.variables
+        ), "Missing sweep_mode in converted dataset"
         assert ds_cf1.attrs["version"] == "1.2", "Incorrect CfRadial version"
-        assert ds_cf1.sizes.mapping == {'time': 5400, 'range': 1832, 'sweep': 11}
+        assert ds_cf1.sizes.mapping == {"time": 5400, "range": 1832, "sweep": 11}
 
         # Call the conversion back to CfRadial2
         dtree_cf2 = xd.transform.to_cfradial2(ds_cf1)
