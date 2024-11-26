5bc2aed8f9410e705ab09555c8e793b93c535e39
Bobby Jackson <rjackson@anl.gov>
2024-11-26 14:15:38 -0600

    Remove print statement

--- xradar/io/backends/hpl.py
+++ xradar/io/backends/hpl.py
@@ -37,14 +37,7 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 import xarray as xr
-<<<<<<< HEAD
-<<<<<<< HEAD
 from xarray import DataTree
-=======
->>>>>>> 617bd34 (Import DataTree from xarray (#218))
-=======
-from xarray import DataTree
->>>>>>> 794c606 (Dtree constructor (#221))
 from xarray.backends.common import AbstractDataStore, BackendArray, BackendEntrypoint
 from xarray.backends.file_manager import CachingFileManager
 from xarray.backends.store import StoreBackendEntrypoint
@@ -658,10 +651,6 @@ def open_hpl_datatree(filename_or_obj, **kwargs):
         for swp in sweeps
     ]
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 794c606 (Dtree constructor (#221))
     dtree: dict = {
         "/": _get_required_root_dataset(ls_ds, optional=optional).rename(
             {"sweep_fixed_angle": "fixed_angle"}
@@ -674,17 +663,3 @@ def open_hpl_datatree(filename_or_obj, **kwargs):
     }
     dtree = _attach_sweep_groups(dtree, ls_ds)
     return DataTree.from_dict(dtree)
-<<<<<<< HEAD
-=======
-    ds.insert(0, xr.Dataset())  # open_dataset(filename_or_obj, group="/"))
-
-    # create datatree root node with required data
-    root = _assign_root(ds)
-    root["fixed_angle"] = ("sweep", [x["sweep_fixed_angle"].values for x in ds[1:]])
-    root["sweep_group_name"] = ("sweep", [x["sweep_group_name"].values for x in ds[1:]])
-    dtree = xr.DataTree(dataset=root, name="root")
-    # return datatree with attached sweep child nodes
-    return _attach_sweep_groups(dtree, ds[1:])
->>>>>>> 617bd34 (Import DataTree from xarray (#218))
-=======
->>>>>>> 794c606 (Dtree constructor (#221))
