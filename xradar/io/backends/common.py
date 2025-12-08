06584a4345cf5bb80f816aefc5c084e57b1eb513
aladinor <aladinor@unal.edu.co>
2025-12-08 17:39:16 -0600

    Fix xarray merge compat FutureWarning
    Add explicit compat='override' parameter to xr.merge() call in _get_subgroup
    function to silence FutureWarning about upcoming xarray default changes.

--- xradar/io/backends/common.py
+++ xradar/io/backends/common.py
@@ -283,7 +283,7 @@ def _get_subgroup(ls_ds: list[xr.Dataset], subdict):
     meta_vars = subdict
     data_vars = {x for xs in [ds.variables.keys() for ds in ls_ds] for x in xs}
     extract_vars = set(data_vars) & set(meta_vars)
-    subgroup = xr.merge([ds[extract_vars] for ds in ls_ds])
+    subgroup = xr.merge([ds[extract_vars] for ds in ls_ds], compat="override")
     for k in subgroup.data_vars:
         rename = meta_vars[k]
         if rename:
